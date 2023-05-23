15c78497752f2350632220409618c9986d23085d
imtritesh <103854328+imtritesh@users.noreply.github.com>
2023-05-23 18:14:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+let select = document.querySelector("#colorSelect");
+let button = document.querySelector(".btn");
+button.addEventListener("click", function(){
+	select.remove(select.selectedIndex);
+})
